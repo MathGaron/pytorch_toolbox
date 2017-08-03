876573e3c9d1a57742d01b239d40c54d05b7ce86
MathGaron <mathieugaron91@gmail.com>
2017-08-03 19:55:20 -0400

    add doc

--- pytorch_toolbox/network/network_base.py
+++ pytorch_toolbox/network/network_base.py
@@ -7,8 +7,20 @@ class NetworkBase(nn.Module):
 
     @abstractmethod
     def forward(self, x):
+        """
+        Define forward as required by nn.module
+        :param x:
+        :return:
+        """
         pass
 
     @abstractmethod
     def loss(self, predictions, targets):
+        """
+        Define criterion on which the train loop will call .backward().
+        Has to return a single value
+        :param predictions: List of network outputs : [output1, output2, ..., outputn]
+        :param targets:     List of target labels : [label1, label2, ..., labeln]
+        :return:
+        """
         pass

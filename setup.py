f09d29011622eb931cdc6dfdcf6d89ca2e665670
MathGaron <mathieugaron91@gmail.com>
2017-08-17 19:42:39 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='pytorch_toolbox',
+    version='0.1dev',
+    packages=['pytorch_toolbox', 'pytorch_toolbox.network', 'pytorch_toolbox.loader']
+)

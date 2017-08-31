0a4ff059f957d9a246722ace474970ef7203a9bd
xiriu <scharcanski@gmail.com>
2017-08-31 13:30:39 -0400

    corrected horizontal flip

--- pytorch_toolbox/transformations/horizontal_flip.py
+++ pytorch_toolbox/transformations/horizontal_flip.py
@@ -3,5 +3,9 @@ import numpy as np
 
 class HorizontalFlip(object):
     def __call__(self, numpy):
-        img = np.flip(numpy, axis=1).copy()
+        random_flip = random.randint(0, 1)
+        if random_flip:
+            img = np.flip(numpy, axis=1).copy()
+        else:
+            img = numpy
         return img

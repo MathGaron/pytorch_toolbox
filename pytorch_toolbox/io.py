3700c920dd235e8ec231f533270bd9c84f403632
MathGaron <mathieugaron91@gmail.com>
2017-09-29 10:29:19 -0400

    rename yaml_loader to yaml_load, add yaml_dump

--- pytorch_toolbox/io.py
+++ pytorch_toolbox/io.py
@@ -9,7 +9,12 @@ This module provide helper function to load/save common data structures
 import yaml
 
 
-def yaml_loader(filepath):
+def yaml_load(filepath):
     with open(filepath, "r") as file_descriptor:
         data = yaml.load(file_descriptor)
         return data
+
+
+def yaml_dump(filepath, data):
+    with open(filepath, 'w') as file_descriptor:
+        yaml.dump(data, file_descriptor)

fe6ca349de26884d44dc5d3ed05e9827136d12d9
jacenfox <jacenfox@gmail.com>
2018-03-13 16:27:36 -0400

    plot embedding, compatible with purely TensorBoard

--- pytorch_toolbox/visualization/tensorboard_logger.py
+++ pytorch_toolbox/visualization/tensorboard_logger.py
@@ -9,8 +9,18 @@ try:
 except ImportError:
     from io import BytesIO  # Python 3.x
 
+# use tensorboardX
+try:
+    from tensorboardX import SummaryWriter
+    import torch
+    import torchvision.utils as vutils
+    use_tensorboardX = True
+except ImportError:
+    use_tensorboardX = False
 
-class TensorboardLogger(object):
+
+class TensorboardLogger_PureTF(object):
+
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
         self.writer_train = tf.summary.FileWriter(os.path.join(log_dir, "train"))
@@ -82,3 +92,62 @@ class TensorboardLogger(object):
         else:
             self.writer_valid.add_summary(summary, step)
             self.writer_valid.flush()
+
+
+class TensorboardLogger_X(object):
+
+    def __init__(self, log_dir):
+        """Create a summary writer logging to log_dir."""
+        self.writer_train = SummaryWriter(os.path.join(log_dir, "train"))
+        self.writer_valid = SummaryWriter(os.path.join(log_dir, "valid"))
+
+    def scalar_summary(self, tag, value, step, is_train=True):
+        """Log a scalar variable."""
+        if is_train:
+            self.writer_train.add_scalar(tag, value, step)
+        else:
+            self.writer_valid.add_scalar(tag, value, step)
+
+    def image_summary(self, tag, images, step, is_train=True):
+        """Log a list of images."""
+        # Input: images - numpy.array [N, 3, H, W]
+        images = torch.from_numpy(images)  # the bridge to TensorboardLogger_PureTF
+        images = vutils.make_grid(images, normalize=True, scale_each=True)
+        if is_train:
+            self.writer_train.add_summary(tag, images, step)
+        else:
+            self.writer_valid.add_summary(tag, images, step)
+
+    def histo_summary(self, tag, values, step, bins='auto', is_train=True):
+        """Log a histogram of the tensor of values."""
+        # Input: values - numpy.array
+        if is_train:
+            self.writer_train.add_histogram(tag, values, step, bins=bins)
+        else:
+            self.writer_valid.add_histogram(tag, values, step, bins=bins)
+
+    def embedding_summary(self, tag, embedding_mat, labels=[], images=None, step=None, is_train=True):
+        """Plot embedding data after a mini-batch."""
+        '''
+        Inputs:
+            embedding_mat - torch.Tensor [N, D]
+            images - torch.Tensor [N, 3, H, W]
+        '''
+        if is_train:
+            self.writer_train.add_embedding(mat=embedding_mat, metadata=labels, label_img=images, global_step=step, tag=tag)
+        else:
+            self.writer_valid.add_embedding(mat=embedding_mat, metadata=labels, label_img=images, global_step=step, tag=tag)
+
+
+if not use_tensorboardX:
+    print('INFO:', 'enjoy embedding visualization with `pip install tensorboardX`')
+
+    class TensorboardLogger(TensorboardLogger_PureTF):
+
+        def __init__(self, log_dir):
+            super(TensorboardLogger_PureTF, self).__init__(log_dir)
+else:
+    class TensorboardLogger(TensorboardLogger_X):
+
+        def __init__(self, log_dir):
+            super(TensorboardLogger, self).__init__(log_dir)
